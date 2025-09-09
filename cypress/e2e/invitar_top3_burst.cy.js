6aabdd15929bfc762fe3d3dc9852ddf54621af97
Lucas <lucaspistolesi07@gmail.com>
2025-09-09 17:34:21 -0300

    implemented e2e tests

--- cypress/e2e/invitar_top3_burst.cy.js
+++ cypress/e2e/invitar_top3_burst.cy.js
@@ -3,6 +3,7 @@
 const BASE = Cypress.env('API_BASE') || 'http://localhost:8080'
 const url = `${BASE}/api/solicitudes/invitar-top3`
 
+
 describe('Burst concurrente (ligero desde Node)', () => {
   it('20 POST concurrentes: no debe haber 5xx', () => {
     cy.task('burstPost', { url, times: 20 }).then(results => {
