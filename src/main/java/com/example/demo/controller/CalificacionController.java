2a3fbdcc1cfdfe9f14dbcb105c652f0805998625
Lucas Agostino <luagostino@uade.edu.ar>
2025-09-22 19:16:59 -0300

    cmabio endpoints

--- src/main/java/com/example/demo/controller/CalificacionController.java
+++ src/main/java/com/example/demo/controller/CalificacionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/prestadores")
+@RequestMapping("/api/prestadores")
 @RequiredArgsConstructor
 @Slf4j
 public class CalificacionController {
